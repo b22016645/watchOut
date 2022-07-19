1ba47a18f16681d3b9e6042689498102c8590f90
seoyoungshinn <doddo1225@gmail.com>
2022-07-19 22:34:29 +0900

    파이어베이스 연동 하였음 프로젝트 이름 : watchOut 프로젝트 ID : watchout-5da32 프로젝트 번호 : 55955354840
    구글 json파일 하나 추가하여서 충돌이 날 수 있음

--- app/src/main/java/route/DetailRoute.kt
+++ app/src/main/java/route/DetailRoute.kt
@@ -52,7 +52,7 @@ object DetailRoute {
 
 
     //현재위치와 미드포인트를 받고, 오차 범위를 받아서
-    //만약에 미드포인트의 오차범위 안에 현재 위치가 없으면 경로이탈이라고 판단한다
+    //현재 위치가 미드포인트의 오차범위 밖에 있으면 경로이탈이라고 판단 -> False 반환.
     fun isRightCource(nowX: Double, nowY: Double, midPointX: Double, midPointY: Double,distanceRange:Double):Boolean{
         //현재위치x와 현재위치 y가 기준위치 x,y와 비교했을때 절댓값 기준으로 오차범위만큼 차이나면 경로이탈로 판단
         var howFarX:Double = abs(nowX - midPointX)
