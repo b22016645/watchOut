feee2e0c15a18c13dc56a1b7da87971eb0a78d4f
seoyoungshinn <doddo1225@gmail.com>
2022-08-15 14:29:58 +0900

    merge

--- app/src/main/java/model/FireBaseObject.kt
+++ app/src/main/java/model/FireBaseObject.kt
@@ -33,12 +33,11 @@ object History { //히스토리 데이터 클래스 (파이어베이스 저장�
         var expWithCar: Int = 0         //위험시설B(차o)에서 이탈한 횟수
         var expTotal: Int = 0           //총 이탈 횟수
 
-<<<<<<< HEAD
         // 선호도 가중치 DB업데이트를 위한 Flag 모음
         var hasDanger : Boolean = false                 //DangerA,B중 하나라도 있으면 true, 기본값은 False
         var hasDangerA: Int? = null                 //DangerA중 하나라도 있으면 notNull, 순서는 엘리베이터-육교-지하보도-계단으로 각 자리수가 시설물의 개수를 나타냄
         var hasDangerB: Int? = null                 //DangerB중 하나라도 있으면 notNull, 순서는 교량-터널-고가도로-대형시설물이동통로 로 각 자리수가 시설물의 개수를 나타냄
-=======
+
         fun init() {
                 arrivedName = null
                 arrivedLat = null
@@ -60,7 +59,7 @@ object History { //히스토리 데이터 클래스 (파이어베이스 저장�
                 expWithCar = 0
                 expTotal = 0
         }
->>>>>>> master
+
 }
 
 object Preference {   //도착지 도착 후 선호도 평가 데이터 베이스 (파이어베이스 저장용)
